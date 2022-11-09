af8f871462278b49fe36514c6911d27afaad34b9
Nadiyamay <ya.nda9301@gmail.com>
2022-11-09 22:01:18 +0400

    Add PUT,DELETE methods, add the ability to apply methods to a single instance.

--- service_app/models.py
+++ service_app/models.py
@@ -6,21 +6,15 @@ from django.urls import reverse
 
 
 class Clients(models.Model):
- # Fields
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this client")
+
     name_of_services = models.CharField(max_length=20, help_text="Enter the name of service", blank=False,)
     service_place = models.CharField(max_length=20, help_text="Select service place", blank=False) 
-    #choices=("at home", "in our salon", "event"))
     client_name=models.CharField(max_length=35,help_text="Enter client name")
     client_email=models.EmailField(help_text="Enter client email", blank=False)
     client_date_of_birth=models.DateField(help_text="Enter client's birth day")
-    #client_id=models.AutoField()
     client_point_of_service=models.IntegerField(help_text="Enter client's point")
     comment=models.TextField(max_length=250)
     ...
-
-    def get_absolute_url(self):
-       return reverse('client-detail', args=[str(self.id)])
 
     def __str__(self):
       return self.client_name
@@ -33,7 +27,7 @@ class Clients(models.Model):
         return self.client_name
 
 class Services(models.Model):
-    id_service = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this service")
+
     SERVICES=[
         ("Make-up","Makeup"),
         ("Nails","Nail service"),
@@ -46,8 +40,6 @@ class Services(models.Model):
     service_duration=models.IntegerField(help_text="Enter service duration", blank=False)
     name_of_master=models.TextField(max_length=30, help_text="Enter the name of master")
     ...
-    def get_absolute_url(self):
-       return reverse('service-detail', args=[str(self.id_service)])
 
     def __str__(self):
         return self.type_of_service
--- service_app/views.py
+++ service_app/views.py
@@ -3,40 +3,87 @@ from django.shortcuts import render
 from rest_framework import  status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from django.http import HttpResponse, JsonResponse
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Clients, Services, Booking 
 from .serializers import ClientsSerializer ,ServicesSerializer, BookingSerializer 
 
-from collections import namedtuple
+
+@api_view(['GET', 'POST'])
+def clients_list(request):
+
+    if request.method == 'GET':
+        clients = Clients.objects.all()
+        serializer = ClientsSerializer(clients, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ClientsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-nt = namedtuple("object", ["model", "serializers"])
-pattern = {
-    "client"  : nt(Clients, ClientsSerializer),
-    "service" : nt(Services, ServicesSerializer),
-    "booking": nt(Booking, BookingSerializer),
-}
+@api_view(['GET', 'PUT', 'DELETE'])
+def clients_detail(request, pk):
 
-@api_view(["GET", "POST"])
-def ListView(request, api_name):
-    print("hello")
-    object =  pattern.get(api_name, None)
-    if object == None:
-        return Response(
-            data   = "Invalid URL",
-            status = status.HTTP_404_NOT_FOUND,
-        )
-    if request.method == "GET":
-        object_list = object.model.objects.all()
-        serializers  = object.serializers(object_list, many=True)
-        return Response(serializers.data)
+    try:
+        client = Clients.objects.get(pk=pk)
+    except Clients.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == "POST":
-        serializers = object.serializers(data=request.data)
-        
-        if serializers.is_valid():
-            serializers.save()
-            return Response(
-                serializers.data, status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-                
+    if request.method == 'GET':
+        serializer = ClientsSerializer(client)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ClientsSerializer(client, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        client.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def services_list(request):
+
+    if request.method == 'GET':
+        services = Services.objects.all()
+        serializer = ServicesSerializer(services, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ServicesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def services_detail(request, pk):
+
+    try:
+        service = Services.objects.get(pk=pk)
+    except Services.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ServicesSerializer(service)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ServicesSerializer(service, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        service.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
