3b86d50f9a02ffd769a10feec1b4b65df34052af
Nadiyamay <ya.nda9301@gmail.com>
2022-12-30 00:09:17 +0400

    Schema created

--- service_app/models.py
+++ service_app/models.py
@@ -8,12 +8,12 @@ from django.urls import reverse
 class Clients(models.Model):
 
     name_of_services = models.CharField(max_length=20, help_text="Enter the name of service", blank=False,)
-    service_place = models.CharField(max_length=20, help_text="Select service place", blank=False) 
+    #service_place = models.CharField(max_length=20, help_text="Select service place", blank=False) 
     client_name=models.CharField(max_length=35,help_text="Enter client name")
-    client_email=models.EmailField(help_text="Enter client email", blank=False)
-    client_date_of_birth=models.DateField(help_text="Enter client's birth day")
-    client_point_of_service=models.IntegerField(help_text="Enter client's point")
-    comment=models.TextField(max_length=250)
+    #client_email=models.EmailField(help_text="Enter client email", blank=False)
+    #client_date_of_birth=models.DateField(help_text="Enter client's birth day")
+    #client_point_of_service=models.IntegerField(help_text="Enter client's point")
+    #comment=models.TextField(max_length=250)
     ...
 
     def __str__(self):
@@ -28,17 +28,18 @@ class Clients(models.Model):
 
 class Services(models.Model):
 
-    SERVICES=[
-        ("Make-up","Makeup"),
-        ("Nails","Nail service"),
-        ("Hair","Hair service"),
-        ("Cosmetology","Cosmetology"),
-        ("Massage","Massage")
-    ]
-    type_of_service=models.CharField(max_length=30, choices=SERVICES, help_text="Select type of service", blank=False)
-    service_price=models.IntegerField(help_text="Enter yuor price")
-    service_duration=models.IntegerField(help_text="Enter service duration", blank=False)
-    name_of_master=models.TextField(max_length=30, help_text="Enter the name of master")
+    #name_of_service=[
+       # ("Make-up","Makeup"),
+        #("Nails","Nail service"),
+        #("Hair","Hair service"),
+        #("Cosmetology","Cosmetology"),
+        #("Massage","Massage")
+    
+    name_of_service=models.CharField(max_length=30, help_text="Add name of service", blank=False)
+    type_of_service=models.CharField(max_length=30, help_text="Select type of service", blank=False)
+    #service_price=models.IntegerField(help_text="Enter yuor price")
+    #service_duration=models.IntegerField(help_text="Enter service duration", blank=False)
+    #name_of_master=models.TextField(max_length=30, help_text="Enter the name of master")
     ...
 
     def __str__(self):
